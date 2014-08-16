e9c7130ee74ff3344bf32f26359283f0d1a1c633
Niklas Wilming <niklaswilming@fastmail.fm>
2014-08-16 15:02:22 +0200

    Sync

--- model.py
+++ model.py
@@ -58,12 +58,12 @@ def oneway_banova(y,X):
         sigma = Uniform('SD lowest', lower=0,upper=10)
         sd_prior = pm.Gamma('SD Prior', 1.01005, 0.1005)
         offset = Normal('offset', mu=0, tau=0.001)
-        #alphas = Normal('alphas', mu=0.0, sd=sd_prior, shape=X.shape[0])
-        #betas = alphas - alphas.mean()
-        #betas = Deterministic('betas', betas)
+        alphas = Normal('alphas', mu=0.0, sd=sd_prior, shape=X.shape[0])
+        betas = alphas - alphas.mean()
+        betas = Deterministic('betas', betas)
 
-        #data = Normal('data', mu= offset + Tns.dot(X.T, betas), 
-        #        sd=sigma, observed=y)
+        data = Normal('data', mu= offset + Tns.dot(X.T, betas), 
+                sd=sigma, observed=y)
     return banova
 
 def run_banova():
@@ -77,11 +77,80 @@ def run_banova():
     start = {'offset': 0.0,
         'alphas': array([0,1,2.])}
     with m:
-        #step = pm.Metropolis()
-        step = pm.NUTS()
-        trace = pm.sample(15000, step, start, tune=1500, njobs=1, progressbar=True)
+        step = pm.Metropolis()
+        #step = pm.NUTS()
+        trace = pm.sample(150000, step, start, tune=1500, njobs=1, progressbar=True)
         pm.traceplot(trace[::2])
     show()
 
+
+def piecewise_predictor(x, split, intercept, slope1, slope2):
+    breakdummy = x<split
+    reg_full = np.array([np.ones(x.shape)*intercept, 
+        slope1*x, 
+        slope2*((x-split)*breakdummy)])
+    return reg_full.sum(0)
+
+
+def piecewise_linear(y,x): 
+    with Model() as pl:
+        split = Uniform('Breakpoint', lower=0, upper=180)
+        intercept = Normal('Offset', mu=y.mean(), sd=y.std()*1000)
+        slopes = Normal('Slope1', mu=0, sd=100, shape=2)
+        mu = piecewise_predictor(x, split, intercept, slopes[0], slopes[1])
+        data = Normal('Data', mu=mu, sd=y.std(), observed=y)
+    return pl
+
+def piecewise_durations(y,x,observer):
+    # Different slopes for different observers.
+    num_observer = len(np.unique(observer))
+    with Model() as pl:
+        split = Uniform('Breakpoint', lower=0, upper=180, shape=num_observer)
+        
+        obs_offset = Normal('Mean_offset', mu=y.mean(), sd=y.std()*1000)
+        obs_slopes1 = Normal('Mean_slope1', mu=0,sd=100)
+        obs_slopes2 = Normal('Mean_slope2', mu=0,sd=100)
+        
+        intercept = Normal('Offsets', mu=obs_offset, sd=y.std()*1000, shape=num_observer)
+        slopes1 = Normal('Slope1', mu=obs_slopes1, sd=100, shape=(num_observer))
+        slopes2 = Normal('Slope2', mu=obs_slopes2, sd=100, shape=(num_observer))
+
+        mu = piecewise_predictor(x, split[observer], 
+                intercept[observer], slopes1[observer], slopes2[observer])
+        data = Normal('Data', mu=mu, sd=y.std(), observed=y)
+    return pl
+
+def run_fixdur():
+    import cPickle
+    dur, fa, obs = cPickle.load(open('durtest.pickle'))
+    print type(obs)
+    m = piecewise_durations(dur.data, fa.data, obs.data-1)
+    with m:
+        step = pm.Metropolis()
+        #step = pm.NUTS()
+        trace = pm.sample(10000, step, {}, tune=150, njobs=2,
+                progressbar=True, trace='text') 
+    return trace
+
+        
+
+def run_pl():
+    x = arange(180)
+    x = concatenate((x,x,x,x))
+    xx = piecewise_predictor(x, 90, 50, 1, 2)
+    y = xx + 20*(np.random.randn(len(x)))
+    y = y-y.mean()
+    m = piecewise_linear(y,x)
+    with m:
+        step = pm.Metropolis()
+        #step = pm.NUTS()
+        trace = pm.sample(2000, step, {}, tune=50, njobs=1, progressbar=True)
+    return trace
+
 if __name__ == '__main__':
-    run_banova()
+    t = run_fixdur()
+    import cPickle
+    cPickle.dump(t, open('trace.pickle', 'w'))
+    #pm.traceplot(trace)
+    #show()
+
